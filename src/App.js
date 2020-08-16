f4d663d2857e6b5a5ed6c8f47870d96da5148edf
Marcus <mojave27@gmail.com>
2020-08-16 15:13:33 -0700

    percentage modifier.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import './main.css';
+import './main.css'
+import './w3.css'
+import './w3-theme.css'
 import Calculator from './components/calculator/Calculator';
+
+const original = {
+    item: 'test item from props',
+    cals: 100,
+    protein: 18,
+    carbs: 3,
+    fat: 1.5
+}
+
 
 function App() {
   return (
-    <div className="main">
-      <div className="main-content">
-        <Calculator />
-      </div>
+    <div className="w3-container w3-theme w3-myfont" style={{minHeight:'100vh'}}>
+        <Calculator item={original} />
     </div>
   );
 }
